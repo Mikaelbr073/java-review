ba99ca6d7a45ce9ddb06c391d3773bf496597c47
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2025-02-20 18:29:21 -0300

    simples_conditional

---
+++ simple_conditional/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int x = 23;
+        //verify if x greater (simple form)
+        if(x > 20){
+            System.out.println("here");
+        };
+
+
+        //verify if y less as x. Else, falls the condition for down
+        int y = 50;
+        if(y < x){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+    }
+}
