b1022bd2c9e2b714587fb8fad25901d480ef9682
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2025-02-21 15:36:38 -0300

    add for

---
+++ for/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int value = 0;
+
+        for (int i = 0; i < 4; i++){
+            value += i;
+            System.out.println(value);
+        }
+    }
+}
