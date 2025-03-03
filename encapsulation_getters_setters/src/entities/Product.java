3a7cbb2a97b37357edda9b214354e4f21849f366
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2025-03-02 22:12:50 -0300

    fix: corretion getters and setters

--- encapsulation_getters_setters/src/entities/Product.java
+++ encapsulation_getters_setters/src/entities/Product.java
@@ -12,6 +12,22 @@ public class Product {
 
     }
 
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public String getName(){
         return name;
     }
